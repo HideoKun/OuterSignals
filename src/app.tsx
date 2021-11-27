2fe28f9c9222e885f30e419a3eac7c725491b866
wojciechmorawski <wo.contact@gmail.com>
2021-11-27 21:13:01 +0100

    css

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
 import React from "react";
 
-export const App = () => <div>react test </div>;
+export const App = () => (
+  <div className="mx-auto text-red-500 bg-red-100 h-full ">react test </div>
+);
