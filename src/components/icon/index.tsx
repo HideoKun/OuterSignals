ab6142766082ef1ea673ed66719292acf7fb7d3e
wojciechmorawski <wo.contact@gmail.com>
2021-12-02 15:41:42 +0100

    feat(icon): support hover

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -28,7 +28,7 @@ const IconComponent: React.FC<Props> = ({
     {...{
       xmlns: "http://www.w3.org/2000/svg",
       // todo - prevent dynamic classes from purge logic
-      className: `w-12 h-12 fill-current stroke-${color}-500 text-${color}-100`,
+      className: `w-12 h-12 transition duration-300 fill-current stroke-${color}-500 text-${color}-100 hover:stroke-${color}-900 hover:text-${color}-300`,
       viewBox: "0 0 24 24",
       shapeRendering: "geometricPrecision",
     }}
