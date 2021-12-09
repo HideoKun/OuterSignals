11462ec9b1d4061585ecf0e5541c8c5beb5d6bb6
s.ancukiewicz <s.ancukiewicz@gmail.com>
2021-12-09 12:48:10 +0100

    slider 2 próba

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,15 @@
 import React, { memo } from "react";
+import { useState } from "react";
 import { Icon } from "./components";
 import "../../slider/src/App.css";
+import {ArrowLeftIcon} from '@heroicons/react/solid'; 
+import {ArrowRightIcon} from '@heroicons/react/solid'; 
 
 const c = {
   root: "flex items-center min-h-full mx-auto justify-center text-center p-5",
 };
 
-export const App = memo(() => (
-  <div className={c.root} style={{ maxWidth: 560 }}>
-    <div className="w-full px-4 py-5 mx-auto bg-white rounded-md shadow-2xl md:p-5">
-      <h1
-        className="mb-4 text-2xl "
-        style={{
-          fontSize: 48,
-          lineHeight: "3rem",
-        }}
-      >
-        Glos Czapli
-      </h1>
-
-      <div className="mt-8">
-        <div className="flex flex-col">
-          <span className="font-bold uppercase">Zespół redakcyjny</span>
-          <span>Ala, Ania, Basia, Bolko, Lili, Zuzia</span>
-        </div>
-        <div className="flex flex-col mt-2 ">
-          <span className="font-bold uppercase">Wsparcie redakcyjne</span>
-          <span>Alicja, Asia, Honorata</span>
-        </div>
-        <div className="flex flex-col mt-2">
-          <span className="font-bold uppercase">Zespół Techniczny</span>
-          <span>Staszek i Wojtek</span>
-        </div>
-
-        <div className="flex justify-around pt-5">
-          <Icon shape="sun" color="sandyBrown" />
-        </div>
-
-        const dataSlider = [
+const dataSlider = [
   { img: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80' },
   { img: 'https://images.unsplash.com/reserve/HgZuGu3gSD6db21T3lxm_San%20Zenone.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
   { img: 'https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' },
@@ -72,7 +44,38 @@ const Slider = ({length}) => {
           })}
       </div>   
   )
-}
+
+export const App = memo(() => (
+  <div className={c.root} style={{ maxWidth: 560 }}>
+    <div className="w-full px-4 py-5 mx-auto bg-white rounded-md shadow-2xl md:p-5">
+      <h1
+        className="mb-4 text-2xl "
+        style={{
+          fontSize: 48,
+          lineHeight: "3rem",
+        }}
+      >
+        Glos Czapli
+      </h1>
+
+      <div className="mt-8">
+        <div className="flex flex-col">
+          <span className="font-bold uppercase">Zespół redakcyjny</span>
+          <span>Ala, Ania, Basia, Bolko, Lili, Zuzia</span>
+        </div>
+        <div className="flex flex-col mt-2 ">
+          <span className="font-bold uppercase">Wsparcie redakcyjne</span>
+          <span>Alicja, Asia, Honorata</span>
+        </div>
+        <div className="flex flex-col mt-2">
+          <span className="font-bold uppercase">Zespół Techniczny</span>
+          <span>Wojtek</span>
+        </div>
+
+        <div className="flex justify-around pt-5">
+          <Icon shape="sun" color="sandyBrown" />
+        </div>))
+
 
         <div className="mt-6 text-justify " style={{ letterSpacing: -0.5 }}>
           <p className="mt-6 font-bold text-center">Cześć!!!</p>
@@ -120,9 +123,3 @@ const Slider = ({length}) => {
     </div>
   </div>
 ));
-
-function App() {
-  return <Slider />
-}
-
-export default App;
